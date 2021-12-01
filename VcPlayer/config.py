b5b56218a5bb84d86ce9ed596e37fe31fe0bfc5c
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-01 10:56:57 +0530

    Create config.py

---
+++ VcPlayer/config.py
@@ -0,0 +1,28 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TheArjvps")
+BG_IMAGE = getenv("BG_IMAGE", "https://te.legra.ph/file/276fe076865d58f67501a.png")
+admins = {}
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Arjvps")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "TheArjvpsChat")
+PROJECT_NAME = getenv("PROJECT_NAME", "VcPlayer")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Akshat7678/VcPlayer")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
